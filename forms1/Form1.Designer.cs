5dec6573e5b8338d31ef3233482b4e72bc66fcb7
Mariana Heloisa Nascimento Damasceno <mariana.heloisa@aluno.ifsp.edu.br>
2023-09-06 12:51:13 -0300

    n

--- forms1/Form1.Designer.cs
+++ forms1/Form1.Designer.cs
@@ -28,23 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnmessage = new System.Windows.Forms.Button();
             this.lblProntuario = new System.Windows.Forms.Label();
             this.txbName = new System.Windows.Forms.TextBox();
-            this.txbEnrollment = new System.Windows.Forms.TextBox();
+            this.txbAtendimento = new System.Windows.Forms.TextBox();
             this.lblNmae = new System.Windows.Forms.Label();
-            this.lblPront = new System.Windows.Forms.Label();
+            this.lblAten = new System.Windows.Forms.Label();
+            this.LblSugestao = new System.Windows.Forms.Label();
+            this.txtSuges = new System.Windows.Forms.TextBox();
+            this.LblRg = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.LblTelefone = new System.Windows.Forms.Label();
+            this.MtbTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.btnmessage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMail = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // btnmessage
-            // 
-            this.btnmessage.Location = new System.Drawing.Point(358, 168);
-            this.btnmessage.Name = "btnmessage";
-            this.btnmessage.Size = new System.Drawing.Size(75, 23);
-            this.btnmessage.TabIndex = 0;
-            this.btnmessage.Text = "clica aqui";
-            this.btnmessage.UseVisualStyleBackColor = true;
-            this.btnmessage.Click += new System.EventHandler(this.btnmessage_Click);
             // 
             // lblProntuario
             // 
@@ -57,44 +56,141 @@
             // 
             // txbName
             // 
-            this.txbName.Location = new System.Drawing.Point(151, 233);
+            this.txbName.Location = new System.Drawing.Point(35, 94);
             this.txbName.Name = "txbName";
-            this.txbName.Size = new System.Drawing.Size(100, 20);
-            this.txbName.TabIndex = 3;
+            this.txbName.Size = new System.Drawing.Size(102, 20);
+            this.txbName.TabIndex = 1;
             // 
-            // txbEnrollment
+            // txbAtendimento
             // 
-            this.txbEnrollment.Location = new System.Drawing.Point(151, 348);
-            this.txbEnrollment.Name = "txbEnrollment";
-            this.txbEnrollment.Size = new System.Drawing.Size(100, 20);
-            this.txbEnrollment.TabIndex = 4;
+            this.txbAtendimento.Location = new System.Drawing.Point(31, 319);
+            this.txbAtendimento.Name = "txbAtendimento";
+            this.txbAtendimento.Size = new System.Drawing.Size(102, 20);
+            this.txbAtendimento.TabIndex = 7;
             // 
             // lblNmae
             // 
             this.lblNmae.AutoSize = true;
-            this.lblNmae.Location = new System.Drawing.Point(146, 190);
+            this.lblNmae.Location = new System.Drawing.Point(40, 78);
             this.lblNmae.Name = "lblNmae";
-            this.lblNmae.Size = new System.Drawing.Size(33, 13);
-            this.lblNmae.TabIndex = 5;
-            this.lblNmae.Text = "nome";
+            this.lblNmae.Size = new System.Drawing.Size(86, 13);
+            this.lblNmae.TabIndex = 0;
+            this.lblNmae.Text = "Digite seu nome:";
             // 
-            // lblPront
+            // lblAten
             // 
-            this.lblPront.AutoSize = true;
-            this.lblPront.Location = new System.Drawing.Point(146, 323);
-            this.lblPront.Name = "lblPront";
-            this.lblPront.Size = new System.Drawing.Size(102, 13);
-            this.lblPront.TabIndex = 6;
-            this.lblPront.Text = "digite seu prontuario";
+            this.lblAten.AutoSize = true;
+            this.lblAten.Location = new System.Drawing.Point(29, 303);
+            this.lblAten.Name = "lblAten";
+            this.lblAten.Size = new System.Drawing.Size(154, 13);
+            this.lblAten.TabIndex = 6;
+            this.lblAten.Text = "Gostou do nosso atendimento?";
+            this.lblAten.Click += new System.EventHandler(this.lblPront_Click);
+            // 
+            // LblSugestao
+            // 
+            this.LblSugestao.AutoSize = true;
+            this.LblSugestao.Location = new System.Drawing.Point(28, 359);
+            this.LblSugestao.Name = "LblSugestao";
+            this.LblSugestao.Size = new System.Drawing.Size(168, 13);
+            this.LblSugestao.TabIndex = 8;
+            this.LblSugestao.Text = "Gostaria de dar alguma sugestão?";
+            // 
+            // txtSuges
+            // 
+            this.txtSuges.Location = new System.Drawing.Point(31, 375);
+            this.txtSuges.Name = "txtSuges";
+            this.txtSuges.Size = new System.Drawing.Size(102, 20);
+            this.txtSuges.TabIndex = 9;
+            // 
+            // LblRg
+            // 
+            this.LblRg.AutoSize = true;
+            this.LblRg.Location = new System.Drawing.Point(40, 236);
+            this.LblRg.Name = "LblRg";
+            this.LblRg.Size = new System.Drawing.Size(80, 13);
+            this.LblRg.TabIndex = 2;
+            this.LblRg.Text = "Digite seu CPF:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(35, 252);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // LblTelefone
+            // 
+            this.LblTelefone.AutoSize = true;
+            this.LblTelefone.Location = new System.Drawing.Point(39, 131);
+            this.LblTelefone.Name = "LblTelefone";
+            this.LblTelefone.Size = new System.Drawing.Size(52, 13);
+            this.LblTelefone.TabIndex = 4;
+            this.LblTelefone.Text = "Telefone:";
+            // 
+            // MtbTelefone
+            // 
+            this.MtbTelefone.Location = new System.Drawing.Point(35, 147);
+            this.MtbTelefone.Mask = "(00) 00000-0000";
+            this.MtbTelefone.Name = "MtbTelefone";
+            this.MtbTelefone.Size = new System.Drawing.Size(102, 20);
+            this.MtbTelefone.TabIndex = 11;
+            // 
+            // btnmessage
+            // 
+            this.btnmessage.Image = global::forms1.Properties.Resources.desenhos_de_stitch_para_colorir_capa_2;
+            this.btnmessage.Location = new System.Drawing.Point(371, 38);
+            this.btnmessage.Name = "btnmessage";
+            this.btnmessage.Size = new System.Drawing.Size(317, 211);
+            this.btnmessage.TabIndex = 10;
+            this.btnmessage.Text = "clique aqui";
+            this.btnmessage.UseVisualStyleBackColor = true;
+            this.btnmessage.Click += new System.EventHandler(this.btnmessage_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Para continuarmos seu FeedBack, faça seu login: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lblMail
+            // 
+            this.lblMail.AutoSize = true;
+            this.lblMail.Location = new System.Drawing.Point(40, 183);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(85, 13);
+            this.lblMail.TabIndex = 13;
+            this.lblMail.Text = "Digite seu E-mail";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(33, 199);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(104, 20);
+            this.textBox3.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblPront);
+            this.ClientSize = new System.Drawing.Size(847, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lblMail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MtbTelefone);
+            this.Controls.Add(this.LblTelefone);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.LblRg);
+            this.Controls.Add(this.txtSuges);
+            this.Controls.Add(this.LblSugestao);
+            this.Controls.Add(this.lblAten);
             this.Controls.Add(this.lblNmae);
-            this.Controls.Add(this.txbEnrollment);
+            this.Controls.Add(this.txbAtendimento);
             this.Controls.Add(this.txbName);
             this.Controls.Add(this.lblProntuario);
             this.Controls.Add(this.btnmessage);
@@ -111,9 +207,18 @@
         private System.Windows.Forms.Button btnmessage;
         private System.Windows.Forms.Label lblProntuario;
         private System.Windows.Forms.TextBox txbName;
-        private System.Windows.Forms.TextBox txbEnrollment;
+        private System.Windows.Forms.TextBox txbAtendimento;
         private System.Windows.Forms.Label lblNmae;
-        private System.Windows.Forms.Label lblPront;
+        private System.Windows.Forms.Label lblAten;
+        private System.Windows.Forms.Label LblSugestao;
+        private System.Windows.Forms.TextBox txtSuges;
+        private System.Windows.Forms.Label LblRg;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label LblTelefone;
+        private System.Windows.Forms.MaskedTextBox MtbTelefone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- forms1/Form1.cs
+++ forms1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,26 +16,87 @@ namespace forms1
         public Form1()
         {
             InitializeComponent();
-        }
+        }   
 
         private void btnmessage_Click(object sender, EventArgs e)
         {
-            string name = txbName.Text;
-            string enrollment = txbEnrollment.Text;
+            Connection connection = new Connection();
+            SqlCommand sqlCommand = new SqlCommand();
 
-            string message = "nome: " + name +
-                             "\nMatricula: " + enrollment;
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"INSERT INTO FeedBack_ VALUES (@name, @Telefone, @Email, @cpf, @atendimento, @sugestao)";
 
-            MessageBox.Show(
-                " sua mae ",
-                "Atenção",
+            sqlCommand.Parameters.AddWithValue("@name", txbName.Text);
+            sqlCommand.Parameters.AddWithValue("@telefone", MtbTelefone.Text);
+            sqlCommand.Parameters.AddWithValue("@email", textBox3.Text);
+            sqlCommand.Parameters.AddWithValue("@cpf", textBox2.Text);
+            sqlCommand.Parameters.AddWithValue("@atendimento", txbAtendimento.Text);
+            sqlCommand.Parameters.AddWithValue("@sugestao", txtSuges.Text);
+
+            sqlCommand.ExecuteNonQuery();
+
+            MessageBox.Show("Cadastro com sucesso",
+                "AVISO",
                 MessageBoxButtons.OK,
-                MessageBoxIcon.Information
+                MessageBoxIcon.Information);
 
-                );
+
+            txbName.Clear();
+            MtbTelefone.Clear();
+            textBox3.Clear();
+            textBox2.Clear();
+            txbAtendimento.Clear();
+            txtSuges.Clear();
+
+            
+
+            //string name = txbName.Text;
+            //string lblAten = txbAtendimento.Text;
+            //string LblSugestao = txtSuges.Text;
+            //string register = textBox2.Text;
+            //string Telephone = MtbTelefone.Text;
+            //string Email = textBox3.Text;
+
+
+            //string message = "nome: " + name +
+            //                "\nTelefone: " + Telephone +
+            //                "\nE-mail : " + Email +
+            //                 "\nCPF: " + register +
+            //                 "\nAtendimento: " + lblAten +
+            //                 "\nSugestão: " + LblSugestao;
+                           
+                             
+            
+            //MessageBox.Show(
+            //    message,
+            //    " Algo esta errado. ",
+            //    MessageBoxButtons.OK,
+            //    MessageBoxIcon.Information
+
+            //    );
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPront_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
